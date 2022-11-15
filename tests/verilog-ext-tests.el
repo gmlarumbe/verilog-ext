f732d10605b62409b5f08e785578e9fb374543ec
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2022-11-15 23:33:07 +0100

    Add imenu ert tests

---
+++ tests/verilog-ext-tests.el
@@ -0,0 +1,34 @@
+;;; verilog-ext-tests.el --- Verilog-Ext ERT tests  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2022 Gonzalo Larumbe
+
+;; Author: Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
+;; URL: https://github.com/gmlarumbe/verilog-ext
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; ERT Tests
+;;
+;;; Code:
+
+
+(require 'verilog-ext-tests-imenu)
+
+
+
+(provide 'verilog-ext-tests)
+
+;;; verilog-ext-tests.el ends here
