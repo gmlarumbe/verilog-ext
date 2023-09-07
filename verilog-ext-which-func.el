ea64c759aebd16aa6cc3cf640b6d5ae5c73fe661
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2023-09-07 20:29:47 +0200

    Remove unnecessary Version header in non-main file

--- verilog-ext-which-func.el
+++ verilog-ext-which-func.el
@@ -4,7 +4,7 @@
 
 ;; Author: Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
 ;; URL: https://github.com/gmlarumbe/verilog-ext
-;; Version: 0.2.0
+
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
